a191af40ed4f8032b8fa98cc394131c65c1d9a4a
Aleck Greenham <greenhama13@gmail.com>
2018-07-05 06:17:29 +0100

    Simplify buildStore

--- src/tests/helpers/buildStore.js
+++ src/tests/helpers/buildStore.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import Thunk from 'redux-thunk';
 
 function buildStore(initialState, reducers) {
-  return createStore(combineReducers(reducers), initialState, compose(applyMiddleware(Thunk)));
+  return createStore(combineReducers(reducers), initialState, applyMiddleware(Thunk));
 }
 
 export default buildStore;
