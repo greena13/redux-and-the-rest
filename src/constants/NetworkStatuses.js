0e8e891a79853b36a30aab30e7033347fbdc2ae0
Phil Allcock <phil.allcock@dubitlimited.com>
2020-02-17 10:45:54 +0000

    Updated 'network_error' to a constant and exported within index

---
+++ src/constants/NetworkStatuses.js
@@ -0,0 +1,9 @@
+/**
+ * The status used when an external API request has errorred at the network level
+ * @type {string}
+ */
+const NETWORK_ERROR = 'NETWORK_ERROR';
+
+export {
+  NETWORK_ERROR,
+};
